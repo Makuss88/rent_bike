992af988e2b3407c53b18d4996a4d7cca6b8d3ee
Dawid <dawidkwiaton123@gmail.com>
2020-01-13 22:49:58 +0100

    create next js

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => (
+    <div>
+        <h1>Hello world!</h1>
+        <p>to jest frontend polaczony z next js zeby był server side rendering</p>
+        <p>backend odpowiada za zapytania bazodanowe i stworzenie api i w sumie tyle</p>
+    </div>
+);
+
+export default Home;
