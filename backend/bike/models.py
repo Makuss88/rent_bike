7ecb0d6ecf477bc18b80300b1f615e339058d3d1
MakussBlue <g.maciborek@wp.pl>
2020-01-14 14:54:19 +0100

    create folder 'templates' - mayby all html in this dir, and creating models, Bike, and User

--- backend/bike/models.py
+++ backend/bike/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django import forms
 
 # Create your models here.
+
+class Bike(models.Models):
+
+    number_bike = models.IntegerField()
+    color_bike = models.CharField(max_length='20')
+    is_free = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.number_bike, 'and his color is', self.color_bike
+
+class User(forms.Form):
+
+    first_name = models.CharField(max_length='50')
+    last_name = models.CharField(max_length='100')
+    email = models.EmailField(max_length='256')
+    password = forms.CharField(max_length=64, widget=forms.PasswordInput)
+    wallet = models.IntegerField
+
+    def __str__(self):
+        return self.last_name, self.first_name
