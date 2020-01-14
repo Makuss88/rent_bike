2c0beb080a4b0357a08ee228aa776868080d8194
MakussBlue <g.maciborek@wp.pl>
2020-01-14 16:40:55 +0100

    add 2 models - User, ane Bike, and give many items to db

--- backend/bike/models.py
+++ backend/bike/models.py
@@ -1,24 +1,26 @@
 from django.db import models
-from django import forms
 
-# Create your models here.
+class Bike(models.Model):
 
-class Bike(models.Models):
-
-    number_bike = models.IntegerField()
-    color_bike = models.CharField(max_length='20')
+    number_bike = models.CharField(max_length=10000)
+    color_bike = models.CharField(max_length=10)
     is_free = models.BooleanField(default=True)
+    image = models.CharField(max_length=1000)
 
     def __str__(self):
-        return self.number_bike, 'and his color is', self.color_bike
+        extra = ''
+        if self.is_free:
+            extra = " - it's free"
+        return "bike no. " + self.number_bike + ' ' + extra
 
-class User(forms.Form):
 
-    first_name = models.CharField(max_length='50')
-    last_name = models.CharField(max_length='100')
-    email = models.EmailField(max_length='256')
-    password = forms.CharField(max_length=64, widget=forms.PasswordInput)
-    wallet = models.IntegerField
+class User(models.Model):
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=256)
+    password = models.CharField(max_length=128)
+    wallet = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.last_name, self.first_name
+        return self.last_name + ' ' + self.first_name
