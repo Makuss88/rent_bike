aa339db93032f954ac90afa41e1115a28850ca4a
MakussBlue <g.maciborek@wp.pl>
2020-01-24 00:42:51 +0100

    we send email to user who can create account

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+import smtplib
 
 
 # Create your views here.
 def register(request):
+
     if request.method == 'POST':
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
@@ -12,11 +14,50 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
 
-        user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,
-                                        password=password1, email=email)
-        user.save()
-        print("HURAY! we have new User!")
-        return redirect('/register/')
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                print("username is taken!")
+            elif User.objects.filter(email=email).exists():
+                print("email is taken!")
+            else:
+                user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username,
+                                                password=password1, email=email)
+                user.save()
+                sendMail(email)
+
+                print("HURAY! we have new User!")
+
+        else:
+            print("bad password....")
+
+        return redirect('/')
 
     else:
         return render(request, 'register.html')
+
+
+def sendMail(mailTo):
+
+    user = 'sqltestsender@gmail.com'
+    password = 'console123!@#'
+
+    mailFrom = 'Poczta automatyczna od serweru!'
+    mailSubject = 'Rent a bike!'
+    mailBody = '''Witaj na moim portalu z wypozyczalnia rowerow!
+    '''
+
+    message = '''From: {}
+    Subject: {}
+
+    {}
+    '''.format(mailFrom, mailSubject, mailBody)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(user, password)
+        server.sendmail(user, mailTo, message)
+        server.close()
+        print('GOOD')
+    except:
+        print('BAD')
--- backend/bike/serializers.py
+++ backend/bike/serializers.py
@@ -5,4 +5,5 @@ from .models import Bike
 class BikeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Bike
-        fields = ("number_bike", "color_bike", "is_free", "image")
+        #fields = ("number_bike", "color_bike", "is_free", "image")
+        fields = '__all__'
